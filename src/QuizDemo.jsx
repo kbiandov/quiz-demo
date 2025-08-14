dbf459b74b74754a445f4cb61c7269aa765fe7de
kbiandov <125369241+kbiandov@users.noreply.github.com>
2025-08-14 14:36:06 +0300

    Update QuizDemo.jsx

--- src/QuizDemo.jsx
+++ src/QuizDemo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "r1eact";
 
 /**
  * QuizDemo – BG UI + Google Sheets + AutoStart + Settings
