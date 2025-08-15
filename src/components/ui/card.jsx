504280a4a9976b34806bfefb2fcf8bf09f84b845
kbiandov <kbiandov@yahoo.com>
2025-08-15 19:37:16 +0300

    del

--- src/components/ui/card.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-export function Card({ className='', children }){ return <div className={`bg-white border border-slate-200 rounded-2xl ${className}`}>{children}</div> }
-export function CardContent({ className='', children }){ return <div className={className}>{children}</div> }
