047f5f7e7f03af84c58d7448e23bf6f5e8b1cc84
kbiandov <kbiandov@yahoo.com>
2025-08-16 13:37:35 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -3,13 +3,7 @@ export const SHEETS = {
   subjects: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd0EZNnDJV2Bf9OpeffMMDLzqyzEB_OZnXW-ePb4G60IhkmGUcHUadKPAny5IVzkfdIbF0GH9YiwHn/pub?gid=567850345&single=true&output=csv",
   lessons: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd0EZNnDJV2Bf9OpeffMMDLzqyzEB_OZnXW-ePb4G60IhkmGUcHUadKPAny5IVzkfdIbF0GH9YiwHn/pub?gid=1551143625&single=true&output=csv",
   questions: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd0EZNnDJV2Bf9OpeffMMDLzqyzEB_OZnXW-ePb4G60IhkmGUcHUadKPAny5IVzkfdIbF0GH9YiwHn/pub?gid=560252204&single=true&output=csv",
-  // TODO: Replace with actual theory sheet tab ID
-  // To get the correct gid:
-  // 1. Open your Google Sheet
-  // 2. Go to the theory tab
-  // 3. Copy the URL and extract the gid parameter
-  // 4. Or use the same gid as one of your existing tabs temporarily
-  theory: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd0EZNnDJV2Bf9OpeffMMDLzqyzEB_OZnXW-ePb4G60IhkmGUcHUadKPAny5IVzkfdIbF0GH9YiwHn/pub?gid=1460999158&single=true&output=csv",
+  theory: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd0EZNnDJV2Bf9OpeffMMDLzqyzEB_OZnXW-ePb4G60IhkmGUcHUadKPAny5IVzkfdIbF0GH9YiwHn/pub?gid=1818357835&single=true&output=csv",
 };
 
 export const STORAGE_KEYS = {
